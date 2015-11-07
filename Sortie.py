a28cd497b01483111193c53332de2f1edeaf99fa
tMercken <thierry.mercken@skynet.be>
2015-11-07 09:31:47 +0100

    Ajout du module sortie

---
+++ Sortie.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*-coding:Latin-1 -*
+
+import os
+from Client import * 
+from CalculCout import nbStationMin  
+
+def RechercheMin (tabCout):
+    minimum = min(tabCout)
+    indMin = tabCout.index(min(tabCout))    
+
+    fSortie = open('Sortie.txt','w')
+    
+    fSortie.write("Coût minimal: ")
+#    fSortie.write(minimum)
+    print >>fSortie, minimum
+    fSortie.write('\n')
+    
+    bestNbStation = nbStationMin + indMin
+    fSortie.write('Nombre de station le plus économique: ')
+#    fSortie.write(bestNbStation)
+    print >>fSortie, bestNbStation
+    fSortie.write('\n')
+    
+    fSortie.close()
+
+
+def SortirTabCout(tabCout):
+    fSortie = open('Sortie.txt','a')
+    fSortie.write('\n')
+    i = 0
+    
+    while i < len(tabCout):
+        fSortie.write('nombre de Station: ')
+        print >>fSortie, nbStationMin + i
+        
+        fSortie.write('  cout : ')
+        print >>fSortie, tabCout[i]
+        fSortie.write('\n')
+        i += 1
+    
+    fSortie.close()
+    
+
+        
